14c91c404d7aa8c87cc60bb781c8e18b50d40e10
vairamuthu-coded <vairamuthu297@gmail.com>
2023-09-15 14:49:43 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -9,29 +9,28 @@ import Nav from './Nav';
 import NewPost from './NewPost';
 import PostPage from './PostPage';
 import EditPost from './EditPost';
-import Post from './Post';
 
  import { DataProvider } from './context/datacontext';
-function App() { 
-  return (
-    <div className="App">
-        <DataProvider>
-          <Header title="Social Media" />
-            <Nav />
-            <Routes>
-                <Route path="/" element={<Home/>}/>             
-                <Route path="post">        
-                      <Route index element={<NewPost/> }/>                  
-                      <Route path=":id" element={<PostPage />} />
-                </Route>
-                <Route path="/edit/:id" element={<EditPost/>}></Route>            
-                <Route path="About" element={<About/> }/>
-                <Route path="*" element={<Missing/>  } />
-            </Routes>
-            <Footer/> 
-        </DataProvider>
-    </div>
-  )
-}
+  function App() { 
+    return (
+      <div className="App">
+          <DataProvider>
+            <Header title="Social Media" />
+              <Nav />
+              <Routes>
+                  <Route path="/" element={<Home/>}/>             
+                  <Route path="post">        
+                        <Route index element={<NewPost/> }/>                  
+                        <Route path=":id" element={<PostPage />} />
+                  </Route>
+                  <Route path="/edit/:id" element={<EditPost/>}></Route>            
+                  <Route path="About" element={<About/> }/>
+                  <Route path="*" element={<Missing/>  } />
+              </Routes>
+              <Footer/> 
+          </DataProvider>
+      </div>
+    )
+  }
 
 export default App;
